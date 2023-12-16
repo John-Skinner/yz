52e17e15bfae8566d445d1857ac7b95d73fe0c32
John Skinner <j.v.skinner.jr@gmail.com>
2023-12-15 21:27:58 -0600

    Add service

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var logger = require('morgan');
+var app = express();
+app.use(function(req,res,next) {
+  const { url,path:routePath } = req;
+  console.log('-- time:',Date.now(), 'URL:'+url + 'PATH:' + routePath);
+  next();
+});
+
+app.use(logger('dev'));
+app.use(express.static('../dist/browser'));
+
+app.listen(4555,()=> {
+  console.log('listening now');
+})
+
