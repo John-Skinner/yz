0cd27797afcb2da6e5bb86e71739297f17344b6d
John Skinner <j.v.skinner.jr@gmail.com>
2023-12-18 19:35:32 -0600

    Adding scores part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {DiceSetComponent} from "./components/dice-set/dice-set.component";
+import {ScoresComponent} from "./components/scores/scores.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DiceSetComponent],
+  imports: [CommonModule, RouterOutlet, DiceSetComponent, ScoresComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  @ViewChild('dicePanel') dicePanel:ElementRef | null = null;
+  @ViewChild('scorePanel') scorePanel:ElementRef | null = null;
   title = 'yahtzee';
+  showDice() {
+    console.log('show dice');
+    let diceDiv = this.dicePanel?.nativeElement as HTMLDivElement;
+    let scoreDiv = this.scorePanel?.nativeElement as HTMLDivElement;
+    scoreDiv.style.visibility='hidden';
+    diceDiv.style.visibility = 'visible';
+
+  };
+  showScores() {
+    console.log('show scores');
+    let diceDiv = this.dicePanel?.nativeElement as HTMLDivElement;
+    let scoreDiv = this.scorePanel?.nativeElement as HTMLDivElement;
+    scoreDiv.style.visibility='visible';
+    diceDiv.style.visibility = 'hidden';
+
+  }
 }
